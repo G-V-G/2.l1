7d3964d8da90d8604a1eb665c09e82316eea5472
MonMon201 <danyavasilinenko213@gmail.com>
2021-03-21 20:40:34 +0200

    add example

---
+++ example/greeting/greeting.go
@@ -0,0 +1,5 @@
+package greeting
+
+func GetGreeting() string {
+	return "Hello world"
+}
---
+++ example/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/G-V-G/2.l1/example/greeting"
+)
+
+func main() {
+	fmt.Println(greeting.GetGreeting())
+}
